a8e6a427cb5515fee68a8aaaad829f7e4ca2ed20
y-oga <47235712+y-oga@users.noreply.github.com>
2019-10-15 18:04:15 +0900

    Add oppositeXX and ownXX.
    primaryXX and secondaryXX have been wrong values.
    These parameters are replaced with oppositeXX and ownXX.

--- scripts/monitor.s/monitor_replica/recover.ps1
+++ scripts/monitor.s/monitor_replica/recover.ps1
@@ -23,26 +23,9 @@ $secondaryHostIp = $env:SECONDARY_HOST_IP_ADDRESS
 # ReplicaServer: active server
 #
 $VMRepInfo = Get-VMReplication -VMName $targetVMName
+$mode = $VMRepInfo.Mode
 $primaryFQDN = $VMRepInfo.PrimaryServer
 $secondaryFQDN = $VMRepInfo.ReplicaServer
-
-#
-# Get information of opposite server (crushed server).
-#
-$tmpPNameList = $primaryFQDN.Split(".")
-$tmpPName = $tmpPNameList[0]
-$oppositeHostname = "null"
-$oppositeFQDN = "null"
-$oppositeIp = "null"
-if ($tmpPName -eq $primaryHostname) {
-    $oppositeHostname = $primaryHostname
-    $oppositeFQDN = $primaryFQDN
-    $oppositeIp = $primaryHostIp
-} else {
-    $oppositeHostname = $secondaryHostname
-    $oppositeFQDN = $secondaryFQDN
-    $oppositeIp = $secondaryHostIp
-}
 
 #
 # Get credential information of both servers.
@@ -53,6 +36,61 @@ $primaryThumbprint = $tmp.Thumbprint
 $tmp = "CN=" + $secondaryFQDN
 $tmp = ls cert:\LocalMachine\My | Where-Object {$_.Subject -eq $tmp}
 $secondaryThumbprint = $tmp.Thumbprint
+
+#
+# Get information of opposite server.
+#
+$tmpPNameList = $primaryFQDN.Split(".")
+$tmpPName = $tmpPNameList[0]
+$ownHostname = "null"
+$ownFQDN = "null"
+$ownIp = "null"
+$ownThumbprint = "null"
+$oppositeHostname = "null"
+$oppositeFQDN = "null"
+$oppositeIp = "null"
+$oppositeThumbprint = "null"
+if ($tmpPName -eq $primaryHostname) {
+    if ($mode -eq "Primary") {
+        $ownHostname = $primaryHostname
+        $ownFQDN = $primaryFQDN
+        $ownIp = $primaryHostIp
+        $ownThumbprint = $primaryThumbprint
+        $oppositeHostname = $secondaryHostname
+        $oppositeFQDN = $secondaryFQDN
+        $oppositeIp = $secondaryHostIp
+        $oppositeThumbprint = $secondaryThumbprint
+    } else {
+        $ownHostname = $secondaryHostname
+        $ownFQDN = $secondaryFQDN
+        $ownIp = $secondaryHostIp
+        $ownThumbprint = $secondaryThumbprint
+        $oppositeHostname = $primaryHostname
+        $oppositeFQDN = $primaryFQDN
+        $oppositeIp = $primaryHostIp
+        $oppositeThumbprint = $primaryThumbprint
+    }
+} else {
+    if ($mode -eq "Primary") {
+        $ownHostname = $secondaryHostname
+        $ownFQDN = $primaryFQDN
+        $ownIp = $secondaryHostIp
+        $ownThumbprint = $secondaryThumbprint
+        $oppositeHostname = $primaryHostname
+        $oppositeFQDN = $secondaryFQDN
+        $oppositeIp = $primaryHostIp
+        $oppositeThumbprint = $primaryThumbprint
+    } else {
+        $ownHostname = $primaryHostname
+        $ownFQDN = $secondaryFQDN
+        $ownIp = $primaryHostIp
+        $ownThumbprint = $primaryThumbprint
+        $oppositeHostname = $secondaryHostname
+        $oppositeFQDN = $primaryFQDN
+        $oppositeIp = $secondaryHostIp
+        $oppositeThumbprint = $secondaryThumbprint
+    }
+}
 
 #
 # Check if opposite returns to a cluster.
@@ -74,7 +112,7 @@ if ($? -eq $False) {
 # Check if opposite server is waiting for VM replication.
 #
 try {
-    $vmRepInfo = Get-VMReplication -VMName $targetVMName -ComputerName $primaryFQDN
+    $vmRepInfo = Get-VMReplication -VMName $targetVMName -ComputerName $oppositeFQDN
 } catch {
     exit 1
 }
@@ -87,15 +125,14 @@ if ($vmRepInfo.State -eq "WaitingForStartResynchronize") {
     }
 
     while (1) {
-        $vmRepInfo = Get-VMReplication -VMName $targetVMName -ComputerName $primaryFQDN
+        $vmRepInfo = Get-VMReplication -VMName $targetVMName -ComputerName $oppositeFQDN
         if ($vmRepInfo.State -eq "WaitingForInitialReplication") {
-            Write-Output $vmRepInfo.State > .\repinfo2.txt
            break
         }
     }
 
     try {
-        Set-VMReplication -VMName $targetVMName -Reverse -ReplicaServerName $primaryFQDN -ComputerName $secondaryFQDN -AuthenticationType "Certificate" -CertificateThumbprint $secondaryThumbprint -Confirm:$False
+        Set-VMReplication -VMName $targetVMName -Reverse -ReplicaServerName $oppositeFQDN -ComputerName $ownFQDN -AuthenticationType "Certificate" -CertificateThumbprint $ownThumbprint -Confirm:$False
     } catch {
         exit 1
     }
@@ -103,13 +140,12 @@ if ($vmRepInfo.State -eq "WaitingForStartResynchronize") {
     while (1) {
         $vmRepInfo = Get-VMReplication -VMName $targetVMName
         if ($vmRepInfo.State -eq "ReadyForInitialReplication") {
-            Write-Output $vmRepInfo.State > .\repinfo3.txt
             break
         }
     }
 
     try {
-        Start-VMInitialReplication -VMName $targetVMName -ComputerName $secondaryFQDN
+        Start-VMInitialReplication -VMName $targetVMName -ComputerName $ownFQDN
     } catch {
         exit 1
     }
@@ -131,19 +167,19 @@ if ($vmRepInfo.State -eq "WaitingForStartResynchronize") {
     }
 
     try {
-        Stop-VMFailover -VMName $targetVMName -ComputerName $secondaryFQDN -Confirm:$False
+        Stop-VMFailover -VMName $targetVMName -ComputerName $ownFQDN -Confirm:$False
     } catch {
         exit 1
     }
 
     try {
-        Resume-VMReplication -VMName $targetVMName -ComputerName $primaryFQDN -Confirm:$False
+        Resume-VMReplication -VMName $targetVMName -ComputerName $oppositeFQDN -Confirm:$False
     } catch {
         exit 1
     }
 
     try {
-        Start-VM -Name $targetVMName -ComputerName $primaryFQDN -Confirm:$False
+        Start-VM -Name $targetVMName -ComputerName $oppositeFQDN -Confirm:$False
     } catch {
         exit 1
     }
