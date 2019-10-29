00f890b1501bffca1d99f3da6cb77392348f1a30
ogata <y-ogata@hg.jp.nec.com>
2019-10-29 13:58:13 +0900

    cleanup

--- failover/network_script/test.bat
+++
@@ -1,3 +0,0 @@
-echo test > .\test.txt
-call .\cluster_config.bat
-Powershell -File .\start.ps1
--- failover/network_script/test.ps1
+++
@@ -1,3 +0,0 @@
-$line = "root@"
-$line = $line + "192.168.145.10"
-ssh -q -o StrictHostKeyChecking=no $line "nmcli c up eth1"
